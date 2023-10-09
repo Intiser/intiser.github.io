e067b92c491eee0d4e00a0a50adb2719723e07b6
Syed Intiser Ahsan <Intiser@users.noreply.github.com>
2023-10-09 18:33:19 +0200

    material

--- src/app/custom/vertcial-tab/vertcial-tab.module.ts
+++ src/app/custom/vertcial-tab/vertcial-tab.module.ts
@@ -4,7 +4,7 @@ import { CommonModule } from '@angular/common';
 import { VertcialTabRoutingModule } from './vertcial-tab-routing.module';
 import { VerticalTabComponent } from './vertical-tab/vertical-tab.component';
 import { VerticalTabListComponent } from './vertical-tab-list/vertical-tab-list.component';
-import { MatListModule } from '@angular/material/list';
+import { MatLegacyListModule as MatListModule } from '@angular/material/legacy-list';
 
 
 @NgModule({
--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import {MatLegacyListModule as MatListModule} from '@angular/material/legacy-list';
 import {MatIconModule} from '@angular/material/icon';
 import { RightSidebarComponent } from './right-sidebar/right-sidebar.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
